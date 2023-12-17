d0e29372eaa3de559d10b0b250ff3f7a1f9489ae
Akhil7379 <154078301+Akhil7379@users.noreply.github.com>
2023-12-17 22:00:24 +0530

    Create variable.c
    my first variable program

---
+++ variable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=8;
+    float b=5.7;
+    char c='A';
+    int d=45;
+    int e=45+4;
+    printf("The value of a is %d \n",a);
+    printf("The value of b is %f \n",b);
+    printf("The value of c is %c \n",c);
+    printf("the sum of a and d is %d \n", a + d);
+    printf("the sum of a and d is %d \n", e);
+    return 0;
+}
